659d844cf39d260d539ad56c3b7e54f88b2abcc8
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-09-26 15:40:56 +0100

    set-up project

---
+++ src/main/java/com/example/ArtGalleryApi/ArtGalleryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArtGalleryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGalleryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ArtGalleryApi/ArtGalleryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArtGalleryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
